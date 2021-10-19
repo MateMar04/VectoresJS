22740f696d078d806230f6fbb7e0bf16aed3a319
MateMar04 <mateo.marchisone@gmail.com>
2021-10-19 08:57:14 -0300

    Adding files

---
+++ script.js
@@ -0,0 +1,20 @@
+const lista = new Array();
+
+var mayor;
+var menor;
+
+var numero
+
+function add() {
+    numero = document.getElementById("leNumero").value;
+    lista.push(numero);
+}
+
+function listar() {
+    document.getElementById("lista").innerHTML = lista;
+}
+
+function mostrarMayor() {
+    mayor = lista.sort[-1]
+    document.getElementById
+}
